43c0a5e9ff69fced95bda0fbcf44b90f1275f736
georgemayne <georgeegyirmayne07@gmail.com>
2024-01-19 00:51:50 +0000

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
-"""
-the number of subscribers (not active users, total subscribers)
-for a given subreddit.
-"""
-
+"""A function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns
-    the number of subscribers (not active users,
-    total subscribers) for a given subreddit.
-    """
-    if not isinstance(subreddit, str):
-        return (0)
-
-    user_agent = {
-        'User-agent': (
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-        )
+    """return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    try:
-        response = requests.get(url, headers=user_agent)
-        response.raise_for_status()
-        results = response.json()
-        return results.get('data', {}).get('subscribers', 0)
-
-    except requests.exceptions.RequestException as req_exc:
-        return (0)
-
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
